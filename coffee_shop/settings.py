0ed44f46f2e04badbad237a5fc4eecf756bea820
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-08-17 05:35:17 -0400

    update staticfiles dirs

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -138,8 +138,8 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "theme" / "static",
-    BASE_DIR / "theme" / "static_src",
+    BASE_DIR / "theme/static",
+    BASE_DIR / "theme/static_src",
 ]
 
 
