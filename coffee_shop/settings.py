03e8a176b4cb317bbd2c3e00d87d42639c299833
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-09-11 00:38:57 -0400

    add debug to enviroment variable

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -17,8 +17,10 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env(DEBUG=(bool, False))
+env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+DEBUG = env.bool("DEBUG")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
