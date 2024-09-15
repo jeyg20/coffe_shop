7f0d2dfc06f86801531c87c4652bce1849622235
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-09-15 00:03:58 -0400

    fix urls when use during debuging

--- backend/coffee_shop/coffee_shop/urls.py
+++ backend/coffee_shop/coffee_shop/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += (path("__reload__/", include("django_browser_reload.urls")),)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
